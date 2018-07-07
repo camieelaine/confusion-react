37557ad5c9aec5330b9a34637e5f8cfc87fa1fcc
Camie DeCovich <camiedecovich@gmail.com>
2018-07-07 10:40:10 -0400

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import './App.css';
 
 //Add boostrap navbar
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       //No longer using this class
@@ -16,7 +26,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );     
   }
