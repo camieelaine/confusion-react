e8a4d0826237a2e6faed7c01d0cc52d668821bec
Camie DeCovich <camiedecovich@gmail.com>
2018-07-08 18:55:52 -0400

    Assignment 1 Task 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,6 +19,29 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
                 );
             
         }
+        renderComments(comments) {
+            if (comments != null)
+            return(
+                <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                {comments.map((comment) => {
+                    return (
+                        <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author} , {comment.date}</p>
+                        </li>
+                    );
+                })}
+                </ul>
+            </div>
+            );
+            else
+            return (
+                <div></div>
+            );
+
+        }
     
         render() {
             if (this.props.dish != null)
@@ -27,7 +50,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
                 <div className="row">
                   {/* <div  className="col-12 col-md-5 m-1"> */}
                     {this.renderDish(this.props.dish)}
-                    {/* {this.renderComments(this.props.dish.comments)} */}
+                    {this.renderComments(this.props.dish.comments)}
                   </div>
                 </div>
 
