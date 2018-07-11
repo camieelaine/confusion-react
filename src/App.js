a01f320b657b3ad586631a87d8ef6dfad3dbba0f
Camie DeCovich <camiedecovich@gmail.com>
2018-07-11 14:44:24 -0400

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 import './App.css';
 
-//Add boostrap navbar
-class App extends Component {
-  
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      dishes: DISHES
-    };
-  }
+  class App extends Component {
 
-  render() {
-    return (
-      //No longer using this class
-      // <div className="App">
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
-      </div>
-    );     
+    render() {
+      return (
+        <div className="App">
+          <Main />
+        </div>
+      );
+    }
   }
-}
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
