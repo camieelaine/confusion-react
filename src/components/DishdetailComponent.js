6c8214f617075e74b54ea71dc7be571e486525ca
Camie DeCovich <camiedecovich@gmail.com>
2018-07-18 16:56:17 -0400

    Combining reducers.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, 
-    BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, 
+    Button, Modal, ModalHeader, ModalBody, ModalFooter, Row, Col, Label } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
         //render details when render dish is called by selectedDish
@@ -19,6 +20,9 @@ import { Link } from 'react-router-dom';
                 );
             
     }
+    const required = (val) => val && val.length;
+    const maxLength = (len) => (val) => !(val) || (val.length <= len);
+    const minLength = (len) => (val) => val && (val.length >= len);
     function RenderComments({comments}) {
             if (comments != null)
             return(
@@ -53,26 +57,93 @@ import { Link } from 'react-router-dom';
             };
         
             this.toggle = this.toggle.bind(this);
+            this.handleSubmit = this.handleSubmit.bind(this);
           }
+          
         
           toggle() {
             this.setState({
               modal: !this.state.modal
             });
+            
           }
+          handleSubmit(values) {
+            console.log('Current State is: ' + JSON.stringify(values));
+            alert('Current State is: ' + JSON.stringify(values));
+            // event.preventDefault();
+        }
+        validate(author) {
+
+            const errors = {
+                author: ''
+            };
+    
+            if (this.state.touched.author && author.length < 3)
+                errors.author = 'Name should be >= 3 characters';
+            else if (this.state.touched.author && author.length > 10)
+                errors.author = 'Name should be <= 10 characters';
+
+            return errors;
+        }
         render(){
         return(
-        <div>
-            <Button onClick={this.toggle} className="fa fa-pencil">Submit Comment</Button>
+        <div className="col-md-5 m-2">
+            <Button onClick={this.toggle} outline color="secondary" className="fa fa-pencil">Submit Comment</Button>
             <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
                 <ModalHeader toggle={this.toggle}>Submit Comment</ModalHeader>
                     <ModalBody>
-                        Form Details here          
+                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Row className="form-group col-md-12">
+                        <Label htmlFor="rating">Rating</Label>
+                                    <Control.select model=".rating" id="rating" name="rating"
+                                        className="form-control">
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    </Control.select>
+                    </Row>
+                    <Row className="form-group col-md-12">
+                                <Label htmlFor="author">Your Name</Label>
+                   
+                                    <Control.text model=".author" id="author" name="author"
+                                        placeholder="Your Name"
+                                        className="form-control"
+                                        validators={{
+                                            required, minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
+                                         />
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be greater than 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                     />
+                               
+                            </Row>
+                            <Row className="form-group col-md-12">
+                                <Label htmlFor="message">Comment</Label>
+    
+                             
+                                    <Control.textarea model=".message" id="message" name="message"
+                                        rows="6"
+                                        className="form-control" />
+                             
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size:10}}>
+                                    <Button type="submit" color="primary">
+                                    Submit
+                                    </Button>
+                                </Col>
+                            </Row>
+                            </LocalForm>        
                 </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.toggle}>Submit</Button>{' '}
-            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-          </ModalFooter>
         </Modal>
         </div>
         );
